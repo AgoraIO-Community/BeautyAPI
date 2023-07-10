9aa9b00b46e0f83b1e2a1fc992dc515a2c009750
xucz <xgfd3@users.noreply.github.com>
2023-07-10 09:54:23 +0800

    [Android]keep scrren on while living .

--- Android/app/src/main/java/io/agora/beauty/demo/FaceUnityActivity.kt
+++ Android/app/src/main/java/io/agora/beauty/demo/FaceUnityActivity.kt
@@ -157,6 +157,7 @@ class FaceUnityActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mBinding.root)
+        window.decorView.keepScreenOn = true
 
         val isCustomCaptureMode =
             intent.getStringExtra(EXTRA_CAPTURE_MODE) == getString(R.string.beauty_capture_custom)
