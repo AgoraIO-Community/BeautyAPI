ad0f3a3b3ab43de774248bd4d9d13261a70b2023
zhangwei <zw0872@163.com>
2024-11-11 15:44:28 +0800

    [Android] update bytedance license

--- Android/app/src/main/java/io/agora/beautyapi/demo/module/bytedance/ByteDanceBeautySDK.kt
+++ Android/app/src/main/java/io/agora/beautyapi/demo/module/bytedance/ByteDanceBeautySDK.kt
@@ -11,7 +11,7 @@ object ByteDanceBeautySDK {
 
     private const val TAG = "ByteDanceBeautySDK"
 
-    private val LICENSE_NAME = "Agora_test_io.agora.test.entfull.licbag"
+    private val LICENSE_NAME = "Agora_test_20241014_20241214_io.agora.test.entfull_4.5.0_2060.licbag"
     private var storagePath = ""
     private var assetsPath = ""
     private var licensePath = ""
