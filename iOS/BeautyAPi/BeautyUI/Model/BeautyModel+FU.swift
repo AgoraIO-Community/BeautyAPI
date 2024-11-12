73b2da126b5df7c519228d5f5eb6f22cf4a467f3
qinhui <>
2024-11-12 20:37:48 +0800

    Modify the beauty effect options

--- iOS/BeautyAPi/BeautyUI/Model/BeautyModel+FU.swift
+++ iOS/BeautyAPi/BeautyUI/Model/BeautyModel+FU.swift
@@ -124,22 +124,6 @@ extension BeautyModel {
         dataArray.append(model)
         
         model = FUBeautyModel()
-        model.path = "face_makeup"
-        model.key = "makeup/xinggan"
-        model.value = 0.5
-        model.name = "show_beauty_item_effect_sexy".beauty_localized
-        model.icon = "meiyan_fgz_cwei"
-        dataArray.append(model)
-        
-        model = FUBeautyModel()
-        model.path = "face_makeup"
-        model.key = "makeup/tianmei"
-        model.value = 0.5
-        model.name = "show_beauty_item_effect_sweet".beauty_localized
-        model.icon = "meiyan_fgz_cwei"
-        dataArray.append(model)
-        
-        model = FUBeautyModel()
         model.path = ""
         model.key = "makeup/diadiatu"
         model.value = 0.8
@@ -147,6 +131,16 @@ extension BeautyModel {
         model.icon = "meiyan_fgz_cwei"
         model.isCombined = true
         dataArray.append(model)
+        
+        model = FUBeautyModel()
+        model.path = ""
+        model.key = "makeup/hunxue"
+        model.value = 0.5
+        model.name = "show_beauty_item_effect_hunxue".beauty_localized
+        model.icon = "meiyan_fgz_hunxue"
+        model.isCombined = true
+        dataArray.append(model)
+        
         
         return dataArray
     }
